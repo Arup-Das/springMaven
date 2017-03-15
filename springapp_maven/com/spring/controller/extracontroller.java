0e42b74ec8968006538c9722919e0e4d098ef807
Arup Kumar Das <arup.das@bjitgroup.com>
2017-03-15 17:11:13 +0600

    Solving Problem related to bean in controller.

--- springapp_maven/com/spring/controller/extracontroller.java
+++ springapp_maven/com/spring/controller/extracontroller.java
@@ -3,14 +3,21 @@ package spring.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import spring.domain.ProductManager;
+
 @Controller
 @RequestMapping("/hello")
 public class extracontroller {
+	
+	@Autowired
+	private ProductManager productManager;
+	
 	@RequestMapping(method = RequestMethod.GET)
     public ModelAndView helloWorld() {
 		System.out.println("Arup Test In extra controller.");
@@ -18,6 +25,7 @@ public class extracontroller {
 
         Map<String, Object> myModel = new HashMap<String, Object>();
         myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
         return new ModelAndView("hello", "model", myModel);
     }
 
