bfb0e10383d22b3b05e8c5377b9daf15a9b7d2b4
Arup Kumar Das <arup.das@bjitgroup.com>
2017-04-10 11:20:22 +0600

    Changes in From and Cintroller

---
+++ springapp_maven/com/spring/controller/formController.java
@@ -0,0 +1,48 @@
+package spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import spring.domain.ProductManager;
+import spring.service.PriceIncrease;
+
+@Controller
+public class formController {
+	
+	@Autowired
+	private ProductManager productManager;
+	
+	@Autowired
+	@Qualifier("priceIncreaseValidator")
+	private Validator validator;
+
+	@InitBinder
+	private void initBinder(WebDataBinder binder) {
+		binder.setValidator(validator);
+	}
+
+	@ModelAttribute("priceincrease")
+	public PriceIncrease createEmployeeModel() {
+		// ModelAttribute value should be same as used in the empSave.jsp
+		return new PriceIncrease();
+	}
+
+	@RequestMapping(value = "/priceincrease", method = RequestMethod.POST)
+	public String saveEmployeeAction(
+			@ModelAttribute("employee") @Validated PriceIncrease priceincrease,
+			BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			System.out.println("Returning empSave.jsp page");
+			return "empSave";
+		}
+		return "empSaveSuccess";
+	}}
