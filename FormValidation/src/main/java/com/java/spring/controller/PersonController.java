31c41b870346360d093b69a096558120a60054e3
Arup Kumar Das <arup.das@bjitgroup.com>
2017-04-13 19:16:39 +0600

    Full Person form made

--- FormValidation/src/main/java/com/java/spring/controller/PersonController.java
+++ FormValidation/src/main/java/com/java/spring/controller/PersonController.java
@@ -1,7 +1,15 @@
 package com.java.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +18,20 @@ import com.java.spring.bean.Person;
 
 @Controller
 public class PersonController {
+	@Autowired
+	@Qualifier("personFormValidator")
+	private Validator validator;
+	
+	@InitBinder
+	private void initBinder(WebDataBinder binder){
+		binder.setValidator(validator);
+	}
+	
+	@ModelAttribute("person")
+	public Person CreatePersonModel(){
+		return new Person();
+	}
+	
 	
 	@RequestMapping(value = "/personform", method = RequestMethod.GET)
     public String formRequest(Model model) {
@@ -18,5 +40,15 @@ public class PersonController {
 		model.addAttribute("person", person);
 		return "person_form";
     }
-
+	
+	@RequestMapping(value = "/personsave", method = RequestMethod.POST)
+	public String saveEmployeeAction(
+			@ModelAttribute("price") @Validated Person person,
+			BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			System.out.println("Returning priceincrease.jsp page");
+			return "person_form";
+		}
+		return "success";
+	}
 }
--- FormValidation/src/main/webapp/WEB-INF/jsp/person_form.jsp
+++ FormValidation/src/main/webapp/WEB-INF/jsp/person_form.jsp
@@ -17,6 +17,42 @@
 </head>
 <body>
 <springForm:form method="POST" modelAttribute="person" action = "personsave">
+	<table>
+		<tr>
+			<td>Name : </td>
+			<td><springForm:input path="name"/></td>
+			<td><springForm:errors path="name" cssClass="error"></springForm:errors></td>
+		</tr>
+		
+		<tr>
+			<td>Email : </td>
+			<td><springForm:input path="email"/></td>
+			<td><springForm:errors path="email" cssClass="error"></springForm:errors></td>
+		</tr>
+		
+		<tr>
+			<td>Phone : </td>
+			<td><springForm:input path="phone"/></td>
+			<td><springForm:errors path="phone" cssClass="error"></springForm:errors></td>
+		</tr>
+		<tr>
+			<td>Gender : </td>
+			<td>
+				<springForm:select path="gender">
+					<springForm:option value="" label="Select Gender"></springForm:option>
+					<springForm:option value="MALE" label="Male"></springForm:option>
+					<springForm:option value="FEMALE" label="Female"></springForm:option>
+				</springForm:select>
+			</td>
+			<td><springForm:errors path="gender" cssClass="error"></springForm:errors></td>
+		</tr>
+		<tr>
+			<td>Birthday : </td>
+			<td><springForm:input path="birthday" placeholder="MM/dd/yyyy"/></td>
+			<td><springForm:errors path="birthday" cssClass="error"></springForm:errors></td>
+		</tr>
+		<tr><td colspan="3"><input type="submit" value="Save Person"></td></tr>
+	</table>
 </springForm:form>
 </body>
 </html>
