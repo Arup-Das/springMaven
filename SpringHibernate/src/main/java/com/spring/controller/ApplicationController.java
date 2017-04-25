954e62b4c63cb4edcd761529d93605385e12f0fa
Arup Kumar Das <arup.das@bjitgroup.com>
2017-04-25 19:09:46 +0600

    No issue.

--- SpringHibernate/src/main/java/com/spring/controller/ApplicationController.java
+++ SpringHibernate/src/main/java/com/spring/controller/ApplicationController.java
@@ -1,10 +1,20 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.model.Employee;
+import com.spring.service.EmployeeService;
 
 @Controller
 public class ApplicationController {
+		      
+	@Autowired
+	MessageSource messageSource;
 	
 	@RequestMapping(value = { "/" })
     public String homePage() {
